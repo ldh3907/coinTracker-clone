72cd672abaa32219948a0842ace17043beca720b
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-11-17 23:37:15 +0900

    styles
    styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  textColor: "black",
-  bgColor: "white",
+  textColor: "#f5f6fa",
+  bgColor: "#0984e3",
+  accentColor: "#4cd137",
 };
